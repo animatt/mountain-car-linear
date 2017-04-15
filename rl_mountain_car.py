fix bug in initial step of gradient ascent and ensure q(S_final,...) equals zero

rl_mountain_car.py
@@ -32,7 +32,9 @@ def bound(position, velocity) :
         else lim.velomax
     pos = np.maximum(position + vel, lim.distmin) if position + vel <= lim.distmax \
         else lim.distmax
-    game_in_progress = True if pos == lim.distmax else False
+    if pos == lim.distmin :
+        vel = 0
+    game_in_progress = True if pos < lim.distmax else False
     return (game_in_progress, pos, vel)
     # velocity_t+1 = bound(velocity_t + A_t - cos(3 * position_t))
     # position_t+1 = bound(position_t + velocity_t+1)
@@ -43,9 +45,9 @@ tile_width = 0.2125
 num_tilings = 8
 memsize = 512
 
-epsilon = 0.1
-alpha = 0.1
-gamma = 0.95
+epsilon = 0.08
+alpha = 0.0005
+gamma = 1
 
 
 theta = [np.zeros((512, 1)), np.zeros((512, 1)), np.zeros((512, 1))]
@@ -63,7 +65,8 @@ def q(S_tiles, A, weights) :
         val += weights[A][index]
     return val
 
-
+count = 1
+avg_ep_length = 0
 converging = True
 while converging :
 
@@ -75,10 +78,12 @@ while converging :
     S2 = tiles.tiles(num_tilings, memsize, (pos2 / tile_width, vel2 / tile_width))
     A2 = policy(S2, theta, epsilon)
 
-    theta[A2][S1] += alpha * (-1 + gamma * q(S2, A2, theta) - q(S1, A1, theta))
+    theta[A1][S1] += alpha * (-1 + gamma * q(S2, A2, theta) - q(S1, A1, theta))
 
+    ep_length = 0
     game_in_progress = True
     while game_in_progress :
+
         pos1, vel1 = pos2, vel2
         S1 = S2
         A1 = A2
@@ -89,8 +94,19 @@ while converging :
         S2 = tiles.tiles(num_tilings, memsize, (pos2 / tile_width, vel2 / tile_width))
         A2 = policy(S2, theta, epsilon)
 
-        theta[A1][S1] += alpha * (-1 + gamma * q(S2, A2, theta) - q(S1, A1, theta))
+        theta[A1][S1] += alpha * (-1 + gamma * game_in_progress * q(S2, A2, theta) - q(S1, A1, theta))
 
+        ep_length += 1
 
+    avg_ep_length = (ep_length + count * avg_ep_length) / (count + 1)
+
+    if count % 10000 == 0 :
+        epsilon /= 1.001
+        alpha /= 1.001
+        print('avg_ep_length:', avg_ep_length)
+        input('Press enter to continue')
+        count = 0; avg_ep_length = 0
+
+    count += 1
 # velocity_t+1 = bound(velocity_t + A_t - cos(3 * position_t))
 # position_t+1 = bound(position_t + velocity_t+1)